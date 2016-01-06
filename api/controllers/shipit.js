1b624f9f7f4357065b499bbd7aab019d876ae960
codingmaven <codingmaven@gmail.com>
2016-01-05 18:49:55 -0800

    # initial commit

--- api/controllers/shipit.js
+++ api/controllers/shipit.js
@@ -18,7 +18,7 @@ async function ups(req, res, next){
     let {tracking_number} = req.params;
     let ups = new UpsClient({licenseNumber: '8CF57BA6F16298A8', userId: 'MITechbilling', password: 'dLp173Vb'});
     ups.requestData({trackingNumber: tracking_number}, function(err, result){
-        if(err) res.send(err);
+        if(err) res.json({error:err});
         else res.json(result);
     });
 }
